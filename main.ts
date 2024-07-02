6ca87037bef0d92f806008480d06c179cc506612
notlegos <roger@mustacich.com>
2024-07-02 14:14:01 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,25 +5,25 @@ radio.onReceivedValue(function (name, value) {
         TPBot.stopCar()
         Connected.ledBrightness(Connected.DigitalRJPin.J1, true)
     } else if (name == "joyLeft") {
-        if (value > 2) {
+        if (value > 1) {
             TPBot.setWheels(-25, 25)
         } else {
             TPBot.stopCar()
         }
     } else if (name == "joyRight") {
-        if (value > 2) {
+        if (value >= 1) {
             TPBot.setWheels(25, -25)
         } else {
             TPBot.stopCar()
         }
-    } else if (name == "joyUp") {
-        if (value > 2) {
+    } else if (name >= "joyUp") {
+        if (value > 1) {
             TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 30)
         } else {
             TPBot.stopCar()
         }
-    } else if (name == "joyDown") {
-        if (value > 2) {
+    } else if (name >= "joyDown") {
+        if (value > 1) {
             TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 30)
         } else {
             TPBot.stopCar()
@@ -34,20 +34,20 @@ radio.onReceivedValue(function (name, value) {
         } else if (value == 6) {
             Connected.laserSensor(Connected.DigitalRJPin.J2, false)
         } else if (value == 2) {
-            TPBot.setServo360(TPBot.ServoList.S2, -100)
-            basic.pause(350)
+            TPBot.setServo360(TPBot.ServoList.S2, 15)
+            basic.pause(15)
             TPBot.setServo360(TPBot.ServoList.S2, 0)
         } else if (value == 3) {
-            TPBot.setServo360(TPBot.ServoList.S2, 100)
-            basic.pause(350)
+            TPBot.setServo360(TPBot.ServoList.S2, -15)
+            basic.pause(15)
             TPBot.setServo360(TPBot.ServoList.S2, 0)
         } else if (value == 1) {
-            TPBot.setWheels(-100, 100)
-            basic.pause(50)
+            TPBot.setWheels(-25, 25)
+            basic.pause(40)
             TPBot.stopCar()
         } else if (value == 4) {
-            TPBot.setWheels(100, -100)
-            basic.pause(50)
+            TPBot.setWheels(25, -25)
+            basic.pause(40)
             TPBot.stopCar()
         } else {
         	
